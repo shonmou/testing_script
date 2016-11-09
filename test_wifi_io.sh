8eb93f5e513db4b1416dd97bf25a3203bc75eec2
Isaac Li <shonmou@gmail.com>
2016-11-09 18:08:44 +0800

    [add]test_wifi_io for read/write 4 bytes testing

---
+++ test_wifi_io.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+target='0x87654321';
+count=0
+
+func_init()
+{
+	$(adb shell rmmod 8723ds.ko)
+	sleep 1
+	$(adb shell insmod /misc/modules/8723ds.ko)
+	sleep 1
+	$(adb shell netcfg wlan0 up)
+	sleep 2
+	$(adb shell rtwpriv wlan0 pm_set ips=0)
+	func_write_0_test
+	func_read_result result
+	echo "driver initial done" $result
+	sleep 1
+}
+
+func_write_0_test()
+{
+	echo "function_write_0_test"
+	$(adb shell rtwpriv wlan0 write 4,0x1b8,0x0)
+}
+
+func_write_pattern_test()
+{
+	echo "function_write_test"
+	$(adb shell rtwpriv wlan0 write 4,0x1b8,$target)
+}
+
+func_read_result()
+{
+	val1=$(adb shell rtwpriv wlan0 read 4,0x1b8 | awk -F":" '{print $2}')
+	eval "$1=$val1"
+}
+
+# Start from here!!
+func_init
+
+while :
+	do
+		func_write_pattern_test
+		func_read_result ret_val
+		echo "read back: "$ret_val
+
+		if echo $val1 | grep -q $target; then
+			echo "PASS!!!!!"
+		else
+			echo "FAIL#####"
+			break
+		fi
+
+	count=$(($count+1))
+	echo "test done: $count"
+	sleep 2
+done
